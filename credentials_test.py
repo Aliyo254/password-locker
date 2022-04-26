4b1b42afa524ce6dda97dccc111d039f9a5f26e7
Aliyo254 <alinurali254@gmail.com>
2022-04-26 09:57:08 +0300

    Class Test

---
+++ credentials_test.py
@@ -0,0 +1,10 @@
+import unittest
+from credentials import Credentials
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
