0903909fc9848019199f0c6b800c760b83df7981
Aliyo254 <alinurali254@gmail.com>
2022-04-26 10:19:13 +0300

    User details

--- user_test.py
+++ user_test.py
@@ -8,15 +8,15 @@ class TestContact(unittest.TestCase):
         '''
         set up method that runs before each Test
         '''
-        self.new_user = User("bashir","billowbashir","123456789")
+        self.new_user = User("alinur","ali","123456789")
     def tearDown(self):
         '''
         method that does clean up after each test case has run.
         '''
         User.user_list=[]
     def test_init(self):
-            self.assertEqual(self.new_user.name,"bashir")
-            self.assertEqual(self.new_user.username,"billowbashir")
+            self.assertEqual(self.new_user.name,"alinur")
+            self.assertEqual(self.new_user.username,"ali")
             self.assertEqual(self.new_user.password,"123456789")
     def test_save_user(self):
         '''
